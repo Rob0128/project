bf62946ed82c4079c5f7c8e25a709605ae34e8d4
Rob0128 <robertjohnhill1@gmail.com>
2022-01-04 20:41:46 +0000

    added class and methods to make object oriented

--- chrtest.py
+++ chrtest.py
@@ -6,47 +6,67 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 
 
+class webscraper(object):
 
 
-WAITROSE_SEARCH_PAGE_URL = "https://www.waitrose.com/ecom/shop/browse/groceries"
-PATIENCE_TIME = 50
-LOAD_MORE_BUTTON_XPATH = '//*[@id="browse-itemsprimary"]/li[2]/button/span/span[2]' 
+    def __init__(self) -> None:
+        super().__init__()
 
-driver = webdriver.Chrome('C:\chromedriver\chromedriver.exe')
+    def open_browser(self):
+        WAITROSE_SEARCH_PAGE_URL = "https://www.waitrose.com/ecom/shop/browse/groceries"
+        PATIENCE_TIME = 50
+        LOAD_MORE_BUTTON_XPATH = '//*[@id="browse-itemsprimary"]/li[2]/button/span/span[2]' 
 
-driver.get(WAITROSE_SEARCH_PAGE_URL)
+        driver = webdriver.Chrome('C:\chromedriver\chromedriver.exe')
 
-#deal with cookie popup
-driver.implicitly_wait(10)
+        driver.get(WAITROSE_SEARCH_PAGE_URL)
 
-driver.find_element_by_xpath('//*[@data-webviewid="accept-cookies"]').click()
+        #deal with cookie popup
+        driver.implicitly_wait(10)
+
+        driver.find_element_by_xpath('//*[@data-webviewid="accept-cookies"]').click()
+
+        return driver
+    
+    def load_pages(self, driver):
+        count = 0
+
+        while True:
+            try:
+                loadMoreButton = driver.find_element_by_xpath("//button[contains(@aria-label,'Load more')]")
+                time.sleep(3)
+                loadMoreButton.click()
+                count += 1
+                time.sleep(4)
+                print(count)
+                if count >= 4:
+                    break
+                
+            except Exception as e:
+                print(e)
+                break
+
+    # elements = driver.find_elements_by_xpath("//header[@class='podHeader___2KpI7']/a")
+    # arr = []
+    # new_str=""
+    # for element in elements:
+    #     href = element.get_attribute("href")
+    #     arr.append(href)
+
+    #     print(arr)
 
 
-while True:
-    try:
-        loadMoreButton = driver.find_element_by_xpath("//button[contains(@aria-label,'Load more')]")
-        time.sleep(2)
-        loadMoreButton.click()
-        time.sleep(5)
-        elements = driver.find_elements_by_xpath("//header[@class='podHeader___2KpI7']/a")
-        arr = []
-        new_str=""
-        for element in elements:
-            href = element.get_attribute("href")
-            arr.append(href)
+            #elements = driver.find_elements_by_class_name("podHeader___2KpI7") 
 
-        print(arr)
-    except Exception as e:
-        print(e)
-        break
+        print("done")
+        print(count)
+        driver.quit()
 
-#elements = driver.find_elements_by_class_name("podHeader___2KpI7") 
-
-print("done")
-time.sleep(10)
-driver.quit()
-
-
+if __name__ == "__main__":
+    scrpr = webscraper
+    driver = webscraper.open_browser(scrpr)
+    webscraper.load_pages(scrpr, driver)
+    
 
 
 
